1fefa03b0c8c89e0453d3d8c40ba6bdf1949d2db
abhay singh chauhan <itsyourabhay@gmail.com>
2024-09-15 15:18:19 +0530

    final changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-
 export default function Home() {
   return (
     <div className="">
-      {/* ... existing code ... */}'asdfkdskj'
+      {/* ... existing code ... */}
       <div>
-      <h1 className="bg-gradient-to-r from-[#14a351] to-[#ffffff] bg-clip-text text-transparent text-7xl">
-        Abhay <br></br>Singh<br></br>Chauhan
-      </h1>      </div>
+        <h1 className="bg-gradient-to-r from-[#14a351] to-[#ffffff] bg-clip-text text-transparent text-7xl">
+          Abhay <br />Singh<br />Chauhan
+        </h1>
+      </div>
       <div>
         
-        </div>
+      </div>
     </div>
   );
 }
